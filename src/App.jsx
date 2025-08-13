fbb3d531f95c3fa54eed72ccc2e84039beb2bcd2
shivamabes <shivam.22b0131041@abes.ac.in>
2025-08-13 21:19:10 +0530

    Added

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
   });
 
   async function reviewCode() {
-    const response = await axios.post("http://localhost:3000/ai/get-review", {
+    const response = await axios.post("https://ai-reviewer-backend.vercel.app/", {
       code,
     });
 
